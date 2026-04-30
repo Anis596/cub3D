6a2eb08a21ac25900300c71d955d1866d4ef48f2
Leo Bardet-Tomczak <lbardet-@c1r7p1.42lehavre.fr>
2026-04-30 03:03:37 +0200

    aaa

---
+++ cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbardet- <lbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 00:12:04 by lbardet-          #+#    #+#             */
+/*   Updated: 2026/04/30 02:27:10 by lbardet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/time.h>
+# include <math.h>
+# include "minilibx-linux/mlx.h"
+# include "Libft/libft.h"
+
+#endif
